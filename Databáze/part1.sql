5412a6a2a4047bbc98fead4b03971f7d9fdfa9a8
Lukkay <162629758+Lukkay@users.noreply.github.com>
2024-04-11 13:51:48 +0200

    Odstranění některých dat databáze part1.sql

--- Databáze/part1.sql
+++ Databáze/part1.sql
@@ -12,10 +12,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dumping database structure for core
-CREATE DATABASE IF NOT EXISTS `core` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `core`;
-
 -- Dumping structure for table core.accounts
 CREATE TABLE IF NOT EXISTS `accounts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
